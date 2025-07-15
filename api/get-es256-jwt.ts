5a879c818ca0c5969c0faaad2df756652b16928e
Jacob Homanics <homanicsjake@gmail.com>
2025-07-14 22:34:15 -0400

    Update get-es256-jwt.ts

--- api/get-es256-jwt.ts
+++ api/get-es256-jwt.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 //import { SignJWT, importPKCS8 } from "jose";
-//
+
 async function issueEs256Jwt(userDid: string) {
   const { SignJWT, importPKCS8 } = await import("jose");
 
