08f94c0c9d017e2cc1f9d836f1c75c6702811faa
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-08-07 13:08:52 -0400

    removed console log

--- api/get-es256-jwt.ts
+++ api/get-es256-jwt.ts
@@ -1,11 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-//import { SignJWT, importPKCS8 } from "jose";
-//
 
 async function issueEs256Jwt(userDid: string) {
   const { SignJWT, importPKCS8 } = await import("jose");
-
-  console.log("PRIVATE KEY:", process.env.ES256_PRIVATE_KEY);
 
   const privateKey = await importPKCS8(
     process.env.ES256_PRIVATE_KEY!.replace(/\\n/g, "\n"),
